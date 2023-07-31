87538e2ec11c8b03da0139babb189a4abefb392e
Azhar <conceptrecall.biz@gmail.com>
2023-07-31 19:51:47 +0500

    stepper

--- src/components/pages/Blog/blog/BlogCard.js
+++ src/components/pages/Blog/blog/BlogCard.js
@@ -1,30 +1,25 @@
-import React from "react";
-import styles from "@/styles/components/blog/blogCard.module.css";
-import Image from "next/image";
-import { blogCardData } from "@/pages/api/utils";
+import React from 'react'
+import styles from '@/styles/components/blog/blogCard.module.css'
+import Image from 'next/image'
 
-const BlogCard = () => {
+const BlogCard = ({ blogCardData }) => {
   return (
     <>
       <div className={styles.blogCards}>
-        {blogCardData.map((item, index) => {
-          return (
-            <>
-              <div key={index} className={styles.blogCard}>
-                <div className={styles.blogCardImage}>
-                  <Image src={item.image} width={200} height={100} />
-                </div>
-                <div className={styles.blogCardContent}>
-                  <h1>{item.question}</h1>
-                  <p>{item.text}</p>
-                </div>
-              </div>
-            </>
-          );
-        })}
+        {blogCardData.map((item, index) => (
+          <div key={index} className={styles.blogCard}>
+            <div className={styles.blogCardImage}>
+              <Image src={item.image} width={200} height={100} />
+            </div>
+            <div className={styles.blogCardContent}>
+              <h1>{item.question}</h1>
+              <p>{item.text}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default BlogCard;
+export default BlogCard
--- src/components/pages/Blog/blog/Blogs.js
+++ src/components/pages/Blog/blog/Blogs.js
@@ -1,45 +1,66 @@
-import React from "react";
-import styles from "@/styles/components/blog/blogs.module.css";
-import Link from "next/link";
-import Search from "/public/assets/images/search.svg";
-import Image from "next/image";
-import BlogCard from "./BlogCard";
-import Faq from "./Faq";
+import React, { useState } from 'react'
+import styles from '@/styles/components/blog/blogs.module.css'
+import Link from 'next/link'
+import Search from '/public/assets/images/search.svg'
+import Image from 'next/image'
+import BlogCard from './BlogCard'
+import Faq from './Faq'
+
 const Blogs = () => {
+  const blogCardData = [
+    {
+      image: '/assets/images/blog1.svg',
+      question: 'How Does Laser Hair Removal Work?',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+    {
+      image: '/assets/images/blog2.svg',
+      question: 'Is Laser Hair Removal Safe?',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+    {
+      image: '/assets/images/blog3.svg',
+      question: 'What Should I do Before and After My Session?',
+      text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    },
+  ]
+
+  const [searchQuery, setSearchQuery] = useState('')
+  const [filteredBlogCardData, setFilteredBlogCardData] = useState(blogCardData)
+
+  const handleSearchInputChange = (event) => {
+    const query = event.target.value
+    setSearchQuery(query)
+
+    // Filter the blogCardData based on the search query
+    const filteredData = blogCardData.filter((item) =>
+      item.question.toLowerCase().includes(query.toLowerCase())
+    )
+
+    setFilteredBlogCardData(filteredData)
+  }
+
   return (
     <>
-      <div className={styles.blogsContainer}>
-        <div className={styles.backgroundimage}>
-          <div className={styles.blogsWrapper}>
-            <div className={styles.blogsContent}>
-              <h1>Blog</h1>
-              <h3>
-                <Link href="/">Home |</Link>{" "}
-                <span>
-                  <Link href="/blog">Blog </Link>
-                </span>
-              </h3>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className={styles.blogsContainer}>
         <div className={styles.blogsContentWrapper}>
           <div className={styles.searchBar}>
             <input
-              type="text"
+              type='text'
               className={styles.searchInput}
-              placeholder="Type your question here …"
+              placeholder='Type your question here …'
+              value={searchQuery}
+              onChange={handleSearchInputChange}
             />
             <button className={styles.searchIcon}>
-              <Image src={Search} alt="Search" width={20} height={20} />
+              <Image src={Search} alt='Search' width={20} height={20} />
             </button>
           </div>
         </div>
       </div>
       <div className={styles.blogsContainer}>
         <div className={styles.blogsContentWrapper}>
-          <BlogCard />
+          <BlogCard blogCardData={filteredBlogCardData} />
         </div>
       </div>
       <div className={styles.blogsContainer}>
@@ -48,7 +69,7 @@ const Blogs = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Blogs;
+export default Blogs
