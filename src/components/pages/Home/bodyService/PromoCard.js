aea2461d3d7c7df3dfe6c275d4c8df15de61d818
abdulyahyadev <dev.abdulyahya@gmail.com>
2023-08-04 15:17:12 -0500

    discount module

--- src/components/pages/Home/bodyService/PromoCard.js
+++ src/components/pages/Home/bodyService/PromoCard.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
-import styles from '@/styles/components/bodyService/PromoCard.module.css'
-import Image from 'next/image'
-import close from '/public/assets/images/circle-xmark.png'
-import Button from '@/src/components/buttons/Button'
+import React, { useState, useEffect } from "react";
+import styles from "@/styles/components/bodyService/PromoCard.module.css";
+import Image from "next/image";
+import close from "/public/assets/images/circle-xmark.png";
+import Button from "@/src/components/buttons/Button";
+import { createAPIEndPoint } from "@/src/config/api";
+import { endPoints } from "@/src/config/endpoints";
 
-const PromoCard = ({ onClose }) => {
+const PromoCard = ({ onClose, handleChecked }) => {
+  const [promoCode, setPromoCode] = useState("teste05570b");
   const handlePromoCardClose = () => {
-    onClose()
-  }
+    onClose();
+  };
+  useEffect(() => {
+    getDiscount();
+  }, []);
+
+  const getDiscount = async () => {
+    try {
+      const { data } = await createAPIEndPoint(
+        endPoints.discounts,
+        true
+      ).fetchAllWithToken();
+      console.log(data, "discount");
+      const unUsedCodes = data.data.filter((item) => !item.attributes.isUsed);
+      console.log(unUsedCodes, "unUsed");
+      const discountVerified = unUsedCodes.find(
+        (obj) => obj.attributes.promoCode === promoCode
+      );
+      if (discountVerified) {
+        // let data = { isUsed: true, ...discountVerified };
+        createAPIEndPoint().update(data);
+      }
+    } catch (error) {
+      console.log(error, "error in discounts");
+    }
+  };
 
   return (
     <>
@@ -16,24 +43,34 @@ const PromoCard = ({ onClose }) => {
           <div className={styles.promoFormContentHeading}>
             <h1>Promo Code</h1>
             <Image
-            style={{cursor: 'pointer'}}
+              style={{ cursor: "pointer" }}
               src={close}
-              alt=''
+              alt=""
               width={25}
               height={25}
               onClick={handlePromoCardClose}
             />
           </div>
           <div className={styles.promoFormContentInput}>
-            <input placeholder='Enter Promo Code' type='text' />
+            <input
+              value={promoCode}
+              onChange={(e) => setPromoCode(e.target.value)}
+              placeholder="Enter Promo Code"
+              type="text"
+            />
           </div>
           <div className={styles.promoFormContentButton}>
-            <Button text='Submit' onClick={() => action()} />
+            <Button
+              text="Submit"
+              onClick={() => {
+                action();
+              }}
+            />
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default PromoCard
+export default PromoCard;
