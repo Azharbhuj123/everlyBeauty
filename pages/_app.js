431f3995d885c8c0b61a499ca8ce8c3325cca122
abdulyahyadev <dev.abdulyahya@gmail.com>
2023-08-10 13:39:04 -0500

    initial data populating

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,22 @@
-import '@/styles/globals.css'
-import { bookingContext } from '@/store/bookingContext'
-import { discountPercentContext } from '@/store/discountPercentContext'
-import React, { useState } from 'react'
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
-import { Toaster } from 'react-hot-toast'
-import Layout from '@/src/components/Layout/Layout'
-import { payableAmountContext } from '@/store/payableAmountContext'
+import "@/styles/globals.css";
+import { bookingContext } from "@/store/bookingContext";
+// import { GetServerSideProps } from "next";
+import { discountPercentContext } from "@/store/discountPercentContext";
+import React, { useState } from "react";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { Toaster } from "react-hot-toast";
+import Layout from "@/src/components/Layout/Layout";
+import { payableAmountContext } from "@/store/payableAmountContext";
 
 export default function App({ Component, pageProps }) {
-  const [booking, setBooking] = useState([])
-  const [discountPercent, setDiscountPercent] = useState(0)
-  const [payable, setPayable] = useState(null)
+  const [booking, setBooking] = useState([]);
+  const [discountPercent, setDiscountPercent] = useState(0);
+  const [payable, setPayable] = useState(null);
 
   return (
     <Layout>
-      <Toaster position='top-right' reverseOrder={false} />
+      <Toaster position="top-right" reverseOrder={false} />
       <payableAmountContext.Provider value={[payable, setPayable]}>
         <discountPercentContext.Provider
           value={[discountPercent, setDiscountPercent]}
@@ -30,5 +31,5 @@ export default function App({ Component, pageProps }) {
         </discountPercentContext.Provider>
       </payableAmountContext.Provider>
     </Layout>
-  )
+  );
 }
